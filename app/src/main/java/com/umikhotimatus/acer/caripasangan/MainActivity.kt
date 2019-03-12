4b936bb44cf2c7712b2d7f833ad1ee7d8235de54
umiuks <umikhotimatus14@gmail.com>
2019-03-12 19:50:48 +0700

    update foto

--- app/src/main/java/com/umikhotimatus/acer/caripasangan/MainActivity.kt
+++ app/src/main/java/com/umikhotimatus/acer/caripasangan/MainActivity.kt
@@ -14,17 +14,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cariPasangan(){
-        val randomCewek = Random().nextInt(3)+1
-        val randomCowok = Random().nextInt(3)+1
+        val randomCewek = Random().nextInt(6)+1
+        val randomCowok = Random().nextInt(16)+1
         val drawableResCewek = when (randomCewek){
             1 ->R.drawable.cewek1
             2 ->R.drawable.cewek2
-            else ->R.drawable.cewek3
+            3 ->R.drawable.cewek3
+            4 ->R.drawable.cewek4
+            5 ->R.drawable.cewek5
+            else ->R.drawable.cewek6
         }
         val drawableResCowok = when (randomCowok){
             1 ->R.drawable.cowok1
             2 ->R.drawable.cowok2
-            else ->R.drawable.cowok3
+            3 ->R.drawable.cowok3
+            4 ->R.drawable.cowok4
+            5 ->R.drawable.cowok5
+            6 ->R.drawable.cowok6
+            7 ->R.drawable.cowok7
+            8 ->R.drawable.cowok8
+            9 ->R.drawable.cowok9
+            10 ->R.drawable.cowok10
+            11 ->R.drawable.cowok11
+            else ->R.drawable.cowok12
 
         }
         cewek.setImageResource(drawableResCewek)
